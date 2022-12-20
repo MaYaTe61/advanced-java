b750383621571224034e6766bdc3731915f8ed3e
MaYaTe61 <fta_ts_1999@hotmail.com>
2022-12-20 13:59:27 +0300

    Changed hello message

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,18 +1,19 @@
 package app;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class App {
-    
-    public static void main(String[] args){
-        //System.out.println("Hello World");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
 
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(10);
         arrayList.add(5);
 
         Iterator<Integer> iterator = arrayList.iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             Integer next = iterator.next();
             System.out.println(next);
         }
