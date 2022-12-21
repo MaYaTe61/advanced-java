2a09afb77605aa86d00e35b244b14569934388f5
MaYaTe61 <fta_ts_1999@hotmail.com>
2022-12-21 09:35:54 +0300

    Changed to Hello World 1

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Hello World 1");
 
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(10);
